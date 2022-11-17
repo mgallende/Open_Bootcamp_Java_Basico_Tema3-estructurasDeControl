7589d141b36b85148888c664908180c370adbc38
Manuel Angel Gómez Allende <81536814+mgallende@users.noreply.github.com>
2022-11-17 23:18:49 +0100

    Update EstructurasDeControlMain.java

--- src/com/mgallende/EstructurasDeControlMain.java
+++ src/com/mgallende/EstructurasDeControlMain.java
@@ -1,2 +1,20 @@
-package com.mgallende;public class EstructurasDeControlMain {
+package com.mgallende;
+/*
+    Ejercicio tema 3 Estructuras de control
+    1 - Crear el paquete,
+    2 - crear la clase
+    3 - crear un bucle que permita concatenar nombres que entregue un array
+  */
+
+public class EstructurasDeControlMain {
+
+    public static void main (String[] args) {
+        String[] nombres = {"Manuel Angel Gómez ", "Pilar García Rodríguez ", "Jorge Gómez García"};
+        String nombresConcatenados = "";
+
+        for (int i = 0; i < nombres.length; i++) {
+            nombresConcatenados += nombres[i];
+        }
+        System.out.println (nombresConcatenados);
+    }
 }
